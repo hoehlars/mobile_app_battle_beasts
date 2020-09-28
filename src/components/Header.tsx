96df060b33068f9457ec1bb686cb72b52227af91
Lars Höhener <hoehelar@students.zhaw.ch>
2020-09-28 20:56:36 +0200

    Linting. Testing workflow.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,44 @@
-
 import * as React from 'react';
-import { Alert, Button, Image, StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-
+import {Alert, Button, Image, StyleSheet, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 interface HeaderProps {
-    toggleDrawer: () => any;
+  toggleDrawer: () => any;
 }
 
 class Header extends React.Component<HeaderProps, {}> {
+  constructor(props: Readonly<HeaderProps>) {
+    super(props);
+  }
 
-    constructor(props: Readonly<HeaderProps>) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <View>
-                <View
-                    style={{
-                        backgroundColor: '#48d1cc',
-                        height: 140,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                    <TouchableOpacity key={1} onPress={this.props.toggleDrawer}>
-                        <Image source={require('./../screens/images/menu-512.png')} style={[styles.headerImage]} />
-                    </TouchableOpacity>
-                    <Text style={{ color: 'white', fontSize: 30 }}>
-                        Battle Beasts
-                    </Text>
-                </View>
-            </View> 
-        )
-    }
+  render() {
+    return (
+      <View>
+        <View
+          style={{
+            backgroundColor: '#48d1cc',
+            height: 140,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <TouchableOpacity key={1} onPress={this.props.toggleDrawer}>
+            <Image
+              source={require('./../screens/images/menu-512.png')}
+              style={[styles.headerImage]}
+            />
+          </TouchableOpacity>
+          <Text style={{color: 'white', fontSize: 30}}>Battle Beasts</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
-  export default Header;
+export default Header;
 
-  const styles = StyleSheet.create({
-    headerImage: {
-      width: 24,
-      height: 24,
-    },
-  });
+const styles = StyleSheet.create({
+  headerImage: {
+    width: 24,
+    height: 24,
+  },
+});
--- src/routes/DrawerNavigation.tsx
+++ src/routes/DrawerNavigation.tsx
@@ -1,61 +1,59 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
 import DeckManagerScreen from '../screens/DeckManagerScreen';
-import AboutTheGameScreen from '../screens/AboutTheGameScreen'
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Image, StyleSheet } from 'react-native';
-
+import AboutTheGameScreen from '../screens/AboutTheGameScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {Image, StyleSheet} from 'react-native';
 
 type DrawerParamList = {
-  Header: undefined
-  Home: undefined
-  DeckManager: undefined
+  Header: undefined;
+  Home: undefined;
+  DeckManager: undefined;
   AboutBattleBeasts: undefined;
 };
 
 const Drawer = createDrawerNavigator<DrawerParamList>();
 
-
-
 const DrawerNavigation: React.FC = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home" 
-          drawerStyle={{
-            backgroundColor: '#21888f'
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerStyle={{
+          backgroundColor: '#21888f',
+        }}
+        drawerContentOptions={{
+          activeTintColor: '#ffffff',
+          itemStyle: {marginVertical: 10},
+        }}>
+        <Drawer.Screen
+          name="Home"
+          options={{
+            drawerIcon: () => (
+              <Image
+                source={require('./../screens/images/iconfinder_home_126572.png')}
+                style={[styles.icon]}
+              />
+            ),
+            title: 'Battle Beasts',
           }}
-          drawerContentOptions={{
-            activeTintColor: '#ffffff',
-            itemStyle: { marginVertical: 10 },
-          }}
-          >
-        <Drawer.Screen 
-        name="Home" 
-        options={{drawerIcon: () => (
-          <Image source={require('./../screens/images/iconfinder_home_126572.png')} style={[styles.icon]} />),
-          title: 'Battle Beasts'
-          }} 
-        component={HomeScreen} 
+          component={HomeScreen}
         />
-        <Drawer.Screen 
-        name="DeckManager" 
-        options={{drawerLabel: "Deck Manager", 
-          title: 'Deck Manager',
-          
-        }} 
-        component={DeckManagerScreen} />
-        <Drawer.Screen 
-        name="AboutBattleBeasts" 
-        options={{drawerLabel: "About the game",
-          title: 'About the game'}} 
-        component={AboutTheGameScreen} />
-
+        <Drawer.Screen
+          name="DeckManager"
+          options={{drawerLabel: 'Deck Manager', title: 'Deck Manager'}}
+          component={DeckManagerScreen}
+        />
+        <Drawer.Screen
+          name="AboutBattleBeasts"
+          options={{drawerLabel: 'About the game', title: 'About the game'}}
+          component={AboutTheGameScreen}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 };
-
 
 const styles = StyleSheet.create({
   icon: {
