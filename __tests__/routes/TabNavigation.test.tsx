c4ca5dc6c9b5bff884891fbd172235be5db33a3d
Lars Höhener <hoehelar@students.zhaw.ch>
2020-10-25 17:01:40 +0100

    Adds TabNavigation test & snapshot.

---
+++ __tests__/routes/TabNavigation.test.tsx
@@ -0,0 +1,18 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import TabNavigation from '../../src/routes/TabNavigation'
+import renderer from 'react-test-renderer'
+
+describe('TabNavigation', () => {
+    it('renders tabnavigation', () => {
+        const tree = renderer.create(<TabNavigation />).toJSON()
+        expect(tree).toMatchSnapshot();
+    });
+})
+
+
+
