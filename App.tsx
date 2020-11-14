3043cebc38d594c700d991e6d32196830b048b30
Lars Höhener <hoehelar@students.zhaw.ch>
2020-11-14 18:40:08 +0100

    App starts now with the LoginNavigator.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import HomeScreenNavigator from './src/screens/Login/HomeScreenNavigator';
+import HomeScreenNavigator from './src/screens/Login/LoginNavigator';
 
 export const App = () => {
   return (
