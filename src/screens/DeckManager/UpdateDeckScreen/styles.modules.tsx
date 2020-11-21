5a3679a906eb939409d2a105714e978966b4bf05
Lars Höhener <hoehelar@students.zhaw.ch>
2020-11-21 13:45:51 +0100

    Styles the card component.

--- src/screens/DeckManager/UpdateDeckScreen/styles.modules.tsx
+++ src/screens/DeckManager/UpdateDeckScreen/styles.modules.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, TouchableHighlightComponent} from 'react-native';
 import {RFPercentage} from 'react-native-responsive-fontsize';
 import theme from '../../../assets/styles/theme.style';
 
@@ -43,13 +43,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   item: {
-    backgroundColor: '#f9c2ff',
-    padding: '10%',
+    backgroundColor: theme.LIGHT_COLOR,
+    padding: '4%',
     marginVertical: '4%',
     marginHorizontal: '10%',
-  },
-  title: {
-    fontSize: RFPercentage(3.5),
+    borderRadius: 10
   },
   error: {
     flex: 0.5,
@@ -69,6 +67,56 @@ const styles = StyleSheet.create({
     fontFamily: 'Nunito-Bold',
     fontSize: RFPercentage(2.5),
   },
+  ImageBackground: {
+    width: '100%',
+    height: 100,
+    flex: 1,
+    flexDirection: 'row'
+  },
+  Image: {
+    borderRadius: 10
+  },
+  IconView: {
+    backgroundColor: theme.LIGHT_COLOR,
+    width: 25,
+    height: 25,
+    borderBottomRightRadius: 10
+  },
+  IconImage: {
+    width: 20,
+    height: 20
+  },
+  CardName: {
+    fontSize: RFPercentage(1),
+    fontFamily: 'Nunito-Bold',
+    color: theme.LIGHT_COLOR,
+    textTransform: 'uppercase',
+    marginLeft: '5%',
+    paddingTop: '5%'
+  },
+  CardDescription: {
+    position: 'absolute',
+    backgroundColor: theme.LIGHT_COLOR,
+    height: '20%',
+    width: '60%',
+    bottom: 0,
+    right: 0,
+    borderTopLeftRadius: 10,
+    flex: 1,
+    flexDirection: 'row'
+  },
+  CardDescriptionPoints: {
+    fontFamily: 'Nunito-Bold',
+    color: theme.DARK_GRAY_COLOR,
+    fontSize: RFPercentage(2.2),
+    marginLeft: '3%'
+  },
+  CardDescriptionIcon: {
+    height: '100%',
+    width: '15%',
+    marginTop: '3%',
+    marginLeft: '3%'
+  }
 });
 
 export default styles;
