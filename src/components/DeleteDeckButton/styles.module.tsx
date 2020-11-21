8e1961e723573b482814e15c6089eca7ed119d6a
Lars Höhener <hoehelar@students.zhaw.ch>
2020-11-21 15:13:58 +0100

    Corrects an import.

--- src/components/DeleteDeckButton/styles.module.tsx
+++ src/components/DeleteDeckButton/styles.module.tsx
@@ -1,6 +1,5 @@
 import {StyleSheet} from 'react-native';
-import {RFPercentage} from 'react-native-responsive-fontsize';
-import theme from '../../../assets/styles/theme.style';
+import theme from '../../assets/styles/theme.style'
 
 const styles = StyleSheet.create({
     RowBack: {
