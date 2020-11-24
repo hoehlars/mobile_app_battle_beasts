0a0d1d3250e460525c32b044a922fa214d50bca5
Ilbien Paul <paulilb@students.zhaw.ch>
2020-11-24 18:12:53 +0100

    added component UserRow with styles for LeaderboardScreen.

---
+++ src/components/UserRow.styles.tsx
@@ -0,0 +1,48 @@
+import {StyleSheet} from 'react-native';
+import theme from '../assets/styles/theme.style';
+import {RFPercentage} from 'react-native-responsive-fontsize';
+
+const styles = StyleSheet.create({
+  UserRow: {
+    width: '100%',
+    height: 'auto',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderWidth: 2,
+    borderColor: 'black',
+    backgroundColor: theme.GRAY_COLOR,
+  },
+  ItemBox: {
+    width: '30%',
+    height: 'auto',
+    backgroundColor: 'green',
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+  ItemBoxRank: {
+    flex: 1,
+    alignItems: 'flex-end',
+    paddingRight: '5%',
+    borderRightWidth: 2,
+    borderRightColor: 'black',
+  },
+  ItemBoxUsername: {
+    flex: 4,
+    paddingRight: '5%',
+    borderRightWidth: 2,
+    borderRightColor: 'black',
+  },
+  ItemBoxSkill: {
+    flex: 2,
+    paddingRight: '5%',
+  },
+  ItemsText: {
+    flex: 1,
+    fontFamily: 'Nunito-Regular',
+    color: 'white',
+    textAlign: 'justify',
+    fontSize: RFPercentage(2),
+  },
+});
+
+export default styles;
