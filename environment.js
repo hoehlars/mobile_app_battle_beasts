2f884645f4b581e60d97f5957cb0233e329c7c43
Lars Höhener <hoehelar@students.zhaw.ch>
2020-12-06 16:30:10 +0100

    Adds card product and deck product to environment.

--- environment.js
+++ environment.js
@@ -1,7 +1,7 @@
 let _Environments = {
   production: {BASE_URL: '', API_KEY: ''},
   staging: {BASE_URL: '', API_KEY: ''},
-  development: {BASE_URL: 'http://localhost:5000', API_KEY: ''},
+  development: {BASE_URL: 'http://localhost:5000', CARD_PRODUCT: 'android.test.purchased', DECK_PRODUCT: 'android.test.purchased'},
 };
 
 var Environment = _Environments.development;
