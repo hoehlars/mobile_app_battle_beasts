9163fc8f20c1d9080d843431ed43402aa155c759
Ilbien Paul <paulilb@students.zhaw.ch>
2020-12-12 20:57:54 +0100

    Added Gamescreen and with rough sizes for avatar, opponent and player

---
+++ src/components/Play/Gameboard/Gameboard.style.tsx
@@ -0,0 +1,29 @@
+/*eslint-disable*/
+import {StyleSheet} from 'react-native';
+import theme from '../../../assets/styles/theme.style';
+
+const styles = StyleSheet.create({
+  Background: {
+      flex: 1,
+      backgroundColor: theme.GRAY_COLOR,
+      flexDirection: 'row',
+  },
+  PlayerAvatar: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  Opponentfield:{
+    flex: 1,
+    backgroundColor: 'green',
+  },
+  Playerfield:{
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  Gameboard: {
+    flex: 5,
+    backgroundColor: 'pink',
+  }
+});
+
+export default styles;
---
+++ src/components/Play/Gameboard/Gameboard.tsx
@@ -0,0 +1,30 @@
+/*eslint-disable*/
+import * as React from 'react';
+import {StyleProp, Text, View, ViewStyle} from 'react-native';
+import Orientation from 'react-native-orientation-locker';
+import styles from './Gameboard.style';
+
+
+class Header extends React.Component<{}, {}> {
+  constructor(props: Readonly<{}>) {
+    super(props);
+  }
+
+  componentDidMount() {
+    Orientation.lockToLandscape();
+  }
+
+  render() {
+    return (
+      <View style={styles.Background}>
+        <View style={styles.PlayerAvatar} />
+        <View style={styles.Gameboard}>
+            <View style={styles.Opponentfield}/>
+            <View style={styles.Playerfield}/>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default Header;
