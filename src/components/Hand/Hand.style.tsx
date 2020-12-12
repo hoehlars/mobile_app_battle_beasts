d300cc4e9f05db6e6a74db9e0a78dc01b7d64064
Lars Höhener <hoehelar@students.zhaw.ch>
2020-12-13 00:28:27 +0100

    Adds hand component & styles.

---
+++ src/components/Hand/Hand.style.tsx
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+import theme from '../../assets/styles/theme.style';
+
+const styles = StyleSheet.create({
+  HandWrapper: {
+    flex: 1,
+    backgroundColor: theme.DARK_GRAY_COLOR
+  },
+  HandCard: {
+    flex: 1,
+    backgroundColor: theme.LIGHT_COLOR,
+    padding: '1%',
+    marginVertical: '2%',
+    marginHorizontal: 5,
+    borderRadius: 10,
+    width: 150,
+    height: '90%'
+  },
+  SilencedCard: {
+    backgroundColor: theme.ERROR_COLOR
+  },
+  SelectMode: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  SelectButton: {
+    flex: 1,
+    borderRadius: 10,
+  }
+});
+
+export default styles;
---
+++ src/components/Hand/Hand.tsx
@@ -0,0 +1,151 @@
+import React from 'react';
+import { ListRenderItemInfo, SafeAreaView, StyleProp, View, TouchableOpacity, Text  } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { Card } from '../../models/card';
+import { CardFlatListData } from '../../models/cardFlatListData';
+import { GameUpdate } from '../../models/gameUpdate';
+import Button from '../Button/Button';
+import CardComponent from '../CardComponent/CardComponent';
+import styles from './Hand.style'
+
+interface HandProps {
+  cards: Card[];
+  boardCards: Card[];
+  hoverCard: (hoveredCardPlayId: number) => void;
+  placeCard: (cardPlayId: number, mode: 'attack' | 'defense') => void;
+  placeSpell: (cardPlayId: number) => void;
+  enableTargetMode: (cardPlayId: number | null) => void;
+  targetMode: number | null;
+  canPlace: boolean;
+  actionPoints: number;
+  gameState: GameUpdate;
+}
+
+interface HandState {
+  selectedCard?: Card;
+  cardsListData: CardFlatListData[];
+}
+
+class Hand extends React.Component<HandProps, HandState> {
+
+  constructor(props: Readonly<HandProps>) {
+    super(props);
+    this.state = {
+      cardsListData: []
+    }
+  }
+
+  componentDidMount() {
+    const cardsFlatList = this.cardToCardListData();
+    this.setState({
+      cardsListData: cardsFlatList
+    })
+  }
+
+  private placeCard(cardPlayId: number, mode: 'attack' | 'defense'): void {
+    this.props.placeCard(cardPlayId, mode);
+    this.setState({ 
+        selectedCard: undefined,
+     });
+  }
+
+  private canPlaceEquipmentCard(equipment: Card): boolean {
+    return !!this.props.boardCards.find(card => card.type === equipment.type && !card.isEquipment);
+  }
+
+  private placeSpell(cardPlayId: number): void {
+    this.props.placeSpell(cardPlayId);
+    this.setState({ selectedCard: undefined });
+  }
+
+  private cardToCardListData(): CardFlatListData[] {
+    const cardFlatListData: CardFlatListData[] = []
+    for(let i = 0; i < this.props.cards.length; i++) {
+      cardFlatListData.push({
+        ...this.props.cards[i],
+        id: i.toString()
+      })
+    }
+    return cardFlatListData;
+  }
+
+  private renderCardInHand(
+    data: ListRenderItemInfo<CardFlatListData>,
+  ): JSX.Element {
+
+     /*[styles.SilencedCard]:
+        !this.props.canPlace ||
+(card.isEquipment && !this.canPlaceEquipmentCard(card)) ||
+this.props.actionPoints < card.actionPoints,
+[styles.SelectedCard]: this.props.canPlace && card.uniquePlayId === this.props.targetMode,*/
+
+    let silenced: any;
+
+    if(!this.props.canPlace || data.item.isEquipment && !this.canPlaceEquipmentCard(data.item) || this.props.actionPoints < data.item.actionPoints) {
+      silenced = styles.SilencedCard
+    }
+    
+    return (
+      <>
+      <CardComponent
+        testID="cardInDeck"
+        card={data.item}
+        onPress={(card: CardFlatListData): void => {
+          if (this.props.canPlace && this.props.actionPoints >= card.actionPoints) {
+            if (card.isEquipment && this.canPlaceEquipmentCard(card)) {
+              this.props.enableTargetMode(card.uniquePlayId);
+            } else if (card.isSpell && !card.needsTarget) {
+              this.setState({ selectedCard: card });
+            } else if (card.isSpell && card.needsTarget) {
+              this.props.enableTargetMode(card.uniquePlayId);
+            } else if (!card.isEquipment) {
+              this.setState({
+                selectedCard: card,
+              });
+
+              if (this.props.targetMode !== null) {
+                this.props.enableTargetMode(null);
+              }
+              
+            }
+          }
+        }}
+        onLongPress={() => {}}
+        styleWrapper={[styles.HandCard, silenced]}
+      />
+      {this.props.canPlace &&
+        this.state.selectedCard === data.item &&
+        data.item.isSpell &&
+      this.placeSpell(data.item.uniquePlayId)}
+
+      {this.props.canPlace && this.state.selectedCard === data.item && !data.item.isSpell ? (
+        <View style={styles.SelectMode}>
+          <Button
+          onPress={() => this.placeCard(data.item.uniquePlayId, 'attack')}
+          title="Attack Mode"
+          styleWrapper={styles.SelectButton}/>
+          <Button
+          onPress={() => this.placeCard(data.item.uniquePlayId, 'defense')}
+          title="Defense Mode"
+          styleWrapper={styles.SelectButton} />
+        </View>
+      ) : null}
+      </>
+    );
+  }
+
+  public render(): React.ReactNode {
+    return (
+          <View style={styles.HandWrapper}>
+            <FlatList
+              horizontal={true}
+              data={this.state.cardsListData}
+              renderItem={(item) => this.renderCardInHand(item)}
+              keyExtractor={(item) => item.id}
+            />
+          </View>
+    );
+  }
+}
+
+export default Hand;
