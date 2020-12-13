351722d4963e450bb9aa2fd2bd269aa8accf1cc2
Lars Höhener <hoehelar@students.zhaw.ch>
2020-12-13 18:31:40 +0100

    Adds disabled property to the button.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,12 +7,14 @@ interface ButtonProps {
   onPress: () => void;
   styleWrapper?: StyleProp<ViewStyle>;
   testID?: string;
+  disabled?: boolean;
 }
 
 class Button extends React.Component<ButtonProps, {}> {
   render(): JSX.Element {
     return (
       <TouchableOpacity
+        disabled={this.props.disabled}
         style={[styles.Button, this.props.styleWrapper]}
         onPress={() => this.props.onPress()}
         testID={this.props.testID}>
