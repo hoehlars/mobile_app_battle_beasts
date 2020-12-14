2762af1042874cba0d82343f7742ce68ad9aff4d
Lars Höhener <hoehelar@students.zhaw.ch>
2020-12-14 18:27:59 +0100

    Adds unlocked card test.

---
+++ __tests__/components/Play/EndBox/UnlockedCard.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import CardComponent from '../../../../src/components/CardComponent/CardComponent';
+import UnlockedCard from '../../../../src/components/Play/EndBox/UnlockedCard';
+
+describe('UnlockedCard', () => {
+  describe('rendering', () => {
+    it('renders the you have won or lost text', () => {
+      const testRenderer = renderer.create(
+        <UnlockedCard rewardCardID={1} />,
+      );
+      const testInstance = testRenderer.root;
+      expect(testInstance.findAllByProps({testID: 'youHaveWonText'})).not.toBeNull();
+    });
+
+    it('renders the card component', () => {
+        const testRenderer = renderer.create(
+          <UnlockedCard rewardCardID={1} />,
+        );
+        const testInstance = testRenderer.root;
+        expect(testInstance.findAllByType(CardComponent)).not.toBeNull();
+      });
+  });
+});
